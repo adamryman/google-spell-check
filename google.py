c7d34dd105d0a11a2373c545caed67c57462788c
Adam Ryman <adamryman@gmail.com>
2016-01-11 17:03:10 -0800

    Add comments

--- google.py
+++ google.py
@@ -12,18 +12,22 @@ def main():
 
     spell_tag = get_spell_tag(page)
 
+    # If the spell tag does not exist or if the text is empty then the input is
+    # spelled correctly as far as Google is concerned, output it
+    # otherwise output the Google spelling
     if spell_tag == None or spell_tag.text == "":
         print(search_request)
     else:
         print(spell_tag.text)
 
+# Get out the tag that has the Google spelling or is empty
 def get_spell_tag(page):
     soup = BeautifulSoup(page.text, 'html.parser')
     spell_tag = soup.find('a', {'class' : 'spell'})
 
     return spell_tag
 
-# Get google html page that has correct spelling
+# Get Google html page that has Google spelling and/or same spelling
 def get_page(search):
     headers = {
         "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0",
@@ -32,7 +36,7 @@ def get_page(search):
     r = requests.get(url, headers=headers)
     return r
 
-# Parse arguments to get search request
+# Parse arguments to get search request for Google spell checking
 def parse_search_request():
     return ' '.join(sys.argv[1:])
 
