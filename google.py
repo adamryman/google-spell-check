8d38b97577b2b8b8d5f10915a02009ab56cd82f8
Adam Ryman <adamryman@gmail.com>
2016-01-11 16:57:05 -0800

    Refactor out main, add User-Agent for correct html

--- google.py
+++ google.py
@@ -5,26 +5,36 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+def main():
+    search_request = parse_search_request()
+
+    page = get_page(search_request)
+
+    spell_tag = get_spell_tag(page)
+
+    if spell_tag == None or spell_tag.text == "":
+        print(search_request)
+    else:
+        print(spell_tag.text)
+
 def get_spell_tag(page):
-    return
+    soup = BeautifulSoup(page.text, 'html.parser')
+    spell_tag = soup.find('a', {'class' : 'spell'})
 
+    return spell_tag
+
+# Get google html page that has correct spelling
 def get_page(search):
-
-    # Gather contents of the page
-    url = 'http://google.com/search?q=' + search
-    print(url)
-    r = requests.get(url)
+    headers = {
+        "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0",
+    }
+    url = 'http://google.com/search?h1=en&q=' + search + "&meta=&gws_rd=ssl"
+    r = requests.get(url, headers=headers)
     return r
 
+# Parse arguments to get search request
 def parse_search_request():
     return ' '.join(sys.argv[1:])
 
 if __name__ == '__main__':
-    search_request = parse_search_request()
-    print(search_request)
-
-    r = get_page(search_request)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    print(soup.find_all('a', {'class' : 'spell'}))
-
-
+    main()
